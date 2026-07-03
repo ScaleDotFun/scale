ae8d8fdc77e689ce0380be23394da84b8fa09487
doom <fissiondotfun@gmail.com>
2026-07-03 05:12:22 -0400

    black screen

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,17 +1,9 @@
 export function App() {
   return (
     <div style={{
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
       height: '100vh',
       width: '100vw',
       background: '#000',
-      color: '#fff',
-      fontFamily: 'monospace',
-      fontSize: '2rem',
-    }}>
-      get farmed
-    </div>
+    }} />
   );
 }
